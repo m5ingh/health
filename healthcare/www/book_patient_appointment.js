492ed592d56df0845bc5f55e74f510b839b735e7
m5ingh <mayank7ingh@gmail.com>
2024-02-25 04:03:50 +0530

    update h settings

--- healthcare/www/book_patient_appointment.js
+++ healthcare/www/book_patient_appointment.js
@@ -267,7 +267,7 @@ function book_appointment(){
 					}
 					frappe.utils.setup_timer(5, 0, $(".time"));
 					setTimeout(() => {
-						window.location.href = url;
+						window.location.href = /view-and-edit-appointments;
 					}, 5000);
 			}
 			$(".portal-full-section").removeClass("freeze-div");
