01b9949193cd61201da33b052abcb94ec4501abb
m5ingh <mayank7ingh@gmail.com>
2024-02-26 00:15:17 +0530

    update h settings

--- healthcare/www/view-and-edit-appointments.py
+++ healthcare/www/view-and-edit-appointments.py
@@ -4,5 +4,5 @@ from frappe import _
 @frappe.whitelist()
 def get_user_appointments(user):
     # Fetch appointments associated with the logged-in user
-    appointments = frappe.get_all('Patient Appointment', filters={'patient': user}, fields=['name', 'date', 'time', 'practitioner'])
+    appointments = frappe.get_all('Patient Appointment', filters={'patient': user}, fields=['name', 'appointment_date', 'appointment_time', 'practitioner'])
     return appointments
