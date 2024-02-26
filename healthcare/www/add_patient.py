6acdd6ea8f58a66cffeade2d5939dc36db203d2e
m5ingh <mayank7ingh@gmail.com>
2024-02-27 00:15:39 +0530

    update h settings

--- healthcare/www/add_patient.py
+++ healthcare/www/add_patient.py
@@ -1,14 +1,16 @@
 # healthcare/www/add_patient.py
 import frappe
+from frappe import _
 
 no_cache = 1
 
+def get_context(context):
+	context.no_cache = 1
+	if frappe.session.user=='Guest':
+		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+
 @frappe.whitelist()
-def add_patient_reg(first_name, gender,email,mobile,context):
-    # Check if the user is logged in
-    context.no_cache = 1
-    if frappe.session.user == 'Guest':
-        frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+def add_patient_reg(first_name, gender,email,mobile):
 
     invite_user = 0
     new_patient = frappe.get_doc({
