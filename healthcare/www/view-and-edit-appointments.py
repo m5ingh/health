6316bb8ed4e0e4a9fe76a4b84943259f9461d0f7
m5ingh <mayank7ingh@gmail.com>
2024-02-28 15:25:26 +0530

    update h settings

--- healthcare/www/view-and-edit-appointments.py
+++ healthcare/www/view-and-edit-appointments.py
@@ -5,14 +5,17 @@ from healthcare.healthcare.doctype.patient.patient import get_patients_from_user
 no_cache = 1
 
 def get_context(context):
+	patients = []
 	context.no_cache = 1
 	if frappe.session.user=='Guest':
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
-
+	else:
+		patients = get_patients_from_user(frappe.session.user)
+	context.patients = patients
 
 @frappe.whitelist()
 def get_user_appointments(user):
-    patients = get_patients_from_user(frappe.session.user)
+
     appointments = frappe.get_all(
         'Patient Appointment',
         filters={'patient': user},
