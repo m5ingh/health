47165eb649b5278a566111568bdc13497855ca81
m5ingh <mayank7ingh@gmail.com>
2024-02-28 22:19:48 +0530

    update h settings

--- healthcare/www/appointments.py
+++ healthcare/www/appointments.py
@@ -1,3 +1,6 @@
 import frappe
+from healthcare.healthcare.doctype.patient.patient import get_patients_from_user
 def get_context(context):
     context.users = frappe.get_list("User", fields=["first_name", "last_name"])
+    patients = get_patients_from_user(frappe.session.user)
+	context.patients = patients
