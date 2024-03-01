8612a86870f23b622ac0b3f5f3c2b398878aaf49
m5ingh <mayank7ingh@gmail.com>
2024-03-02 03:44:03 +0530

    update h settings

--- healthcare/www/appointments.py
+++ healthcare/www/appointments.py
@@ -8,3 +8,10 @@ def get_context(context):
     context.users = frappe.get_list("User", fields=["first_name", "last_name"])
     patients = get_patients_from_user(frappe.session.user)
     context.patients = patients
+
+    appointments = frappe.get_all(
+        "Patient Appointment",
+        filters={'patient': patients},
+        fields=['full_name', 'appointment_date', 'appointment_time', 'practitioner']
+    )
+    context.appointments = appointments
