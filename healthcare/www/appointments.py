dbac8a8b95e0582559d89ab5600fff1d43996644
m5ingh <mayank7ingh@gmail.com>
2024-03-03 02:23:58 +0530

    update h settings

--- healthcare/www/appointments.py
+++ healthcare/www/appointments.py
@@ -17,6 +17,6 @@ def get_context(context):
     appointments = frappe.get_all(
         "Patient Appointment",
         filters={'patient_name': ('in', [patient.get("full_name") for patient in patients])},
-        fields=['id','appointment_date', 'appointment_time', 'practitioner_name  ']
+        fields=['naming_series','appointment_date', 'appointment_time', 'practitioner_name']
     )
     context.appointments = appointments
