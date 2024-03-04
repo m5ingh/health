2aa65aad36132f2d8504e957172107efc5861aa9
m5ingh <mayank7ingh@gmail.com>
2024-03-04 06:11:37 +0530

    update h settings

--- healthcare/www/appointments.py
+++ healthcare/www/appointments.py
@@ -5,15 +5,9 @@ no_cache = 1
 
 def get_context(context):
     context.no_cache = 1
-    context.users = frappe.get_list("User", fields=["first_name", "last_name"])
     patients = get_patients_from_user(frappe.session.user)
     context.patients = patients
 
-    for patient in patients:
-        patient_name = patient.get("full_name")  # Access the full_name attribute using get method
-        # Do something with patient_name if needed
-
-    # Corrected the loop to access each patient's name
     appointments = frappe.get_all(
         "Patient Appointment",
         filters={'patient_name': ('in', [patient.get("full_name") for patient in patients])},
