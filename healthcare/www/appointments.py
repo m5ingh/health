1c5b10f67b3f7e7a35cdd72d12e74c226191a94a
m5ingh <mayank7ingh@gmail.com>
2024-03-04 06:48:21 +0530

    update h settings

--- healthcare/www/appointments.py
+++ healthcare/www/appointments.py
@@ -6,7 +6,7 @@ no_cache = 1
 def get_context(context):
     context.no_cache = 1
     if frappe.session.user=='Guest':
-		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
+        frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
     patients = get_patients_from_user(frappe.session.user)
     context.patients = patients
 
